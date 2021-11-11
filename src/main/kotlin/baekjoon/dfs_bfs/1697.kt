676445676a99a4c3c637939ef645ef52c98179d3
KLDAJI <kldaji@naver.com>
2021-11-11 09:01:56 +0900

    feat: 1697 - complete

--- src/main/kotlin/baekjoon/dfs_bfs/1697.kt
+++ src/main/kotlin/baekjoon/dfs_bfs/1697.kt
@@ -1,10 +1,9 @@
 package baekjoon.dfs_bfs
 
 import java.util.*
-import kotlin.math.max
 
-fun traverse(queue: Queue<Pair<Int, Int>>, visited: Array<Boolean>, nDest: Int, size: Int, count: Int) {
-    if (nDest in 0 until 2 * size && !visited[nDest]) {
+fun traverse(queue: Queue<Pair<Int, Int>>, visited: Array<Boolean>, nDest: Int, count: Int) {
+    if (nDest in 0 until 100001 && !visited[nDest]) {
         queue.add(Pair(nDest, count + 1))
         visited[nDest] = true
     }
@@ -14,9 +13,8 @@ fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
     val (n, k) = br.readLine().split(" ").map { it.toInt() }
-    val size = max(n, k)
     val queue: Queue<Pair<Int, Int>> = LinkedList()
-    val visited = Array(size * 2) { false }
+    val visited = Array(100001) { false }
     queue.add(Pair(n, 0))
     visited[n] = false
     var result = 0
@@ -26,9 +24,9 @@ fun main() {
             result = count
             break
         }
-        traverse(queue, visited, dest + 1, size, count)
-        traverse(queue, visited, dest - 1, size, count)
-        traverse(queue, visited, dest * 2, size, count)
+        traverse(queue, visited, dest + 1, count)
+        traverse(queue, visited, dest - 1, count)
+        traverse(queue, visited, dest * 2, count)
     }
     bw.write("$result")
     br.close()

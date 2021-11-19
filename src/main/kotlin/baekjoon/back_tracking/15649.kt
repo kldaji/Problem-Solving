76b96a4c102c5c595b8d58d8c08097a28ebff21a
KLDAJI <kldaji@naver.com>
2021-11-20 00:00:00 +0900

    feat: 15649 - incomplete

--- src/main/kotlin/baekjoon/back_tracking/15649.kt
+++ src/main/kotlin/baekjoon/back_tracking/15649.kt
@@ -1,9 +1,16 @@
 package baekjoon.back_tracking
 
+data class Group(val id: String, val selected: Boolean)
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
 
+    val group1 = Group("1", false)
+    val group2 = Group("1", false)
+    val groupList = listOf(group1, group2)
+
+    println("${group1 == group2}")
     br.close()
     bw.close()
 }
